9dc2db09deb3008b8138323cb58f56f1acea4cbd
Patrick Treyer <p.treyer@web.de>
2017-05-09 11:07:59 +0200

    removes duplicated abstract declaration

--- src/Price.java
+++ src/Price.java
@@ -6,7 +6,7 @@
  * To change this template use File | Settings | File Templates.
  */
 interface Price {
-    abstract int getPriceCode();
-    abstract double getCharge(int daysRented);
-    abstract int getFrequentRenterPoints(int daysRented);
+    int getPriceCode();
+    double getCharge(int daysRented);
+    int getFrequentRenterPoints(int daysRented);
 }
