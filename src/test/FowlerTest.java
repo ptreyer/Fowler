640161c59acb095a94501404e2e8ef104630a805
Patrick Treyer <p.treyer@web.de>
2017-05-12 20:27:53 +0200

    adds tests

--- src/test/FowlerTest.java
+++ src/test/FowlerTest.java
@@ -4,22 +4,53 @@ import main.Customer;
 import main.Movie;
 import main.Rental;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FowlerTest {
 
+    Movie movie1;
+    Movie movie2;
+    Customer customer;
+
+    @Before
+    public void setup() {
+        movie1 = new Movie("Matrix", 0);
+        movie2 = new Movie("Lab", 2);
+
+        customer = new Customer("Patrick Treyer");
+        customer.addRental(new Rental(movie1, 5));
+        customer.addRental(new Rental(movie2, 2));
+    }
+
     @Test
-    public void test(){
-        Movie movieHdR = new Movie("Matrix", 0);
-        Movie movieBB = new Movie("Child", 2);
+    public void testCharge(){
+       Assert.assertEquals(1.5, movie2.getCharge(2),0);
+    }
 
-        Customer customer = new Customer("Patrick Treyer");
-        customer.addRental(new Rental(movieHdR, 5));
-        customer.addRental(new Rental(movieBB, 2));
+    @Test
+    public void testFrequentRenterPoints(){
+        Assert.assertEquals(1, movie1.getFrequentRenterPoints());
+    }
 
+    @Test
+    public void testTitle(){
+        Assert.assertEquals( "Lab", movie2.getTitle());
+    }
+
+    @Test
+    public void testMovieEquals(){
+        Assert.assertFalse(movie1.equals(movie2));
+    }
+
+    @Test
+    public void testCstomerEquals(){
+        Assert.assertFalse(customer.equals(new Customer("Patrick Treyer")));
+    }
+
+    @Test
+    public void testStatement() {
         String statement = customer.statement();
-        System.out.println(statement);
-
         Assert.assertNotNull(statement);
     }
 
